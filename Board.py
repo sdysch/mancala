6254b639278ead48333188780bb41fba811d45f8
Samuel Dezso Dysch <sdysch94@gmail.com>
2022-01-02 13:32:20 +0000

    Add some validity checks

--- Board.py
+++ Board.py
@@ -32,4 +32,61 @@ class Board:
         board_string += '\n'
 
         board_string += (offset + offset_space) * ' ' + ' '.join([str(v) for v in self.player_one_cups])
+
         return board_string
+
+    def check_valid_player(self, player_number):
+        ''' Check is player number is valid '''
+
+        if player_number not in [1, 2]:
+            raise ValueError('Player number must be either 1 or 2')
+
+    def check_valid_bucket(self, bucket):
+        ''' Check if chosen bucket is valid '''
+
+        valid_buckets = [v + 1 for v in range(self._NCUPS)]
+        if bucket not in [valid_buckets]:
+            raise ValueError('Invalid bucket number')
+
+    def get_player_cups(self, player_number):
+        ''' get the cups for player_number '''
+
+        self.check_valid_player(player_number)
+        return self.player_two_cups if player_number == 2 else self.player_one_cups
+
+    def get_opponent_cups(self, player_number):
+        ''' get the cups for opponent '''
+
+        self.check_valid_player(player_number)
+        return self.player_one_cups if player_number == 2 else self.player_two_cups
+
+    def get_player_goal(self, player_number):
+        ''' get the goal for player_number '''
+
+        self.check_valid_player(player_number)
+        player_goal = self.player_one_goal if player_number == 1 else self.player_two_goal
+
+        return player_goal
+
+    def is_valid_move(self, player_number, bucket):
+        ''' Checks if a player's bucket has at least 1 marble in it, therefore enabling a valid move '''
+
+        self.check_valid_player(player_number)
+        self.check_valid_bucket(bucket)
+
+        player_cups = self.get_player_cups
+
+        return player_cups[bucket - 1] == 0
+
+    def make_player_move(self, player_number, bucket):
+        ''' Iterate through board until no valid moves are left '''
+
+        self.check_valid_player(player_number)
+        self.check_valid_bucket(bucket)
+
+        if player_number == 1:
+            player_cups   = self.player_one_cups
+            opponent_cups = self.player_two_cups
+        else:
+            player_cups   = self.player_two_cups
+            opponent_cups = self.player_one_cups
--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 Exploring different strategies for [mancala](https://en.wikipedia.org/wiki/Mancala)
 
 * Player 1 is the player to move first
+
+## Bucket numbering
+* A board is represented like so:
+```
+		6 6 6 6 6 6
+[0]                      [0]
+		6 6 6 6 6 6
+
+```
+* Player 1's buckets are numbered from left to right
+* Player 2's buckets are numbered from _right to left_
+* Player 1's goal is on the right, player 2's is on the left
+```
+Bucket  6 5 4 3 2 1
+
+		6 6 6 6 6 6
+[0]                      [0]
+		6 6 6 6 6 6
+		
+Bucket  1 2 3 4 5 6
+
+```
