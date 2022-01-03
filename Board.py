0418268eb2fef52f24aebec6dba4a132ded5df89
Samuel Dezso Dysch <sdysch94@gmail.com>
2022-01-03 18:09:29 +0000

    Still not quite working

--- Board.py
+++ Board.py
@@ -4,6 +4,7 @@ class Board:
     _NCUPS = 6
 
     def __init__(self):
+        # TODO - other initial configurations? can just be set after class is initiated
         self.player_one_cups = self._NCUPS * [6]
         self.player_two_cups = self._NCUPS * [6]
 
@@ -86,59 +87,64 @@ class Board:
         self.check_valid_bucket(bucket)
 
         # get player/opponent cups and goals
-        player_cups   = self.get_player_cups(player_number)
+        if player_number == 1:
+            marbles = self.player_one_cups[bucket - 1]
+            self.player_one_cups[bucket - 1] = 0
+        else:
+            marbles = self.player_two_cups[bucket - 1]
+            self.player_two_cups[bucket - 1] = 0
 
         # if there are no marbles, this is not a valid move and we return
-        marbles = player_cups[bucket - 1]
         if marbles == 0:
             return
 
-        opponent_cups = self.get_opponent_cups(player_number)
-        player_goal   = self.get_player_goal(player_number)
-        opponent_goal = self.get_opponent_goal(player_number)
-
         # starting position
-        position = bucket
-
-        # reset this bucket to 0
-        player_cups[bucket - 1] = 0
+        if bucket == self._NCUPS:
+            position = self._NCUPS
+        else:
+            position = bucket + 1
 
         # whose cups we are updating
-        updating_cups = player_number
+        original_player = player_number
+        updating_cups   = player_number
 
         # flag to keep track if we ended in a player's goal
         ended_in_goal = False
 
         # distribute the marbles across the board
         for _ in range(marbles):
-
             # we have reached the edge of the board
-            if position == self._NCUPS:
-                player_goal += 1
+            if position == self._NCUPS + 1:
+                ended_in_goal = True
 
-                # now we are at the edge of the board, switch whose goal we are updating
+                # only update player's goal if we have been iterating over their cups
+                # otherwise, we need to update the initial cup of the other player
+                if updating_cups == original_player:
+                    position = 1
+                    if updating_cups == 1:
+                        self.player_one_goal += 1
+                    else:
+                        self.player_two_goal += 1
+
+                else:
+                    position = 2
+                    if updating_cups == 1:
+                        self.player_two_cups[0] += 1
+                    else:
+                        self.player_one_cups[0] += 1
+
+                # switch whose goal we are updating
                 updating_cups = 1 if updating_cups == 2 else 2
 
-                # reset position for next player
-                ended_in_goal = True
-                position = 0
-
+            # not yet reached the end of the board
             else:
+                if updating_cups == 1:
+                    self.player_one_cups[position - 1] += 1
+                else:
+                    self.player_two_cups[position - 1] += 1
+
+                # update position for next turn
                 ended_in_goal = False
                 position += 1
-
-                if updating_cups == 1:
-                    player_cups[position - 1] += 1
-                elif updating_cups == 2:
-                    opponent_cups[position - 1] += 1
-                else:
-                    raise ValueError('updating_cups is neither 1 nor 2, this should not happen')
-
-        # store the updated player and opponent cups/goals
-        self.player_one_cups = player_cups
-        self.player_two_cups = opponent_cups
-
-        self.player_one_goal = player_goal
-        self.player_two_goal = opponent_goal
 
         return ended_in_goal
--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def main(args):
     test_board = Board()
     #print(test_board)
 
-    test_board.player_one_cups = [7, 0, 0, 0, 0, 0]
-    test_board.player_two_cups = [1, 0, 0, 0, 0, 0]
+    # test_board.player_one_cups = [0, 0, 0, 0, 0, 6]
+    # test_board.player_two_cups = [0, 0, 0, 0, 0, 0]
+    # print(test_board)
+    # test_board.make_player_move(1, 6)
+    # print(test_board)
+
+    #test_board.player_two_cups = [0, 0, 0, 0, 0, 7]
+    test_board.player_two_cups = [0, 0, 0, 0, 0, 8]
+    test_board.player_one_cups = [0, 0, 5, 0, 0, 0]
     print(test_board)
-    test_board.make_player_move(1, 1)
+    #test_board.make_player_move(1, 3)
+    test_board.make_player_move(2, 6)
     print(test_board)
 
 if __name__ == '__main__':
