4f420b9d55700ec45ef2615523a7601b94bd9a61
Samuel Dezso Dysch <sdysch94@gmail.com>
2022-01-29 13:05:26 +0000

    Tidy up

--- core/players/MaxScorePlayer.py
+++ core/players/MaxScorePlayer.py
@@ -18,52 +18,27 @@ class MaxScorePlayer(Player):
         self.seed = seed
         self.rng.seed(seed)
 
-    def move_score(self, board):
+    def score(self, board):
         return board.get_player_goal(self.player)
 
     def move(self, board):
         """Choose the move from board to maximise the player's score.
             If there are multiple possible moves, then the choice is random"""
 
-        from copy import deepcopy
-
         if board.player != self.player:
             raise ValueError(f'board player is {board.player} and MaxScorePlayer is {self.player}. Please check your logic')
 
-        #print(f'Board: \n{board}')
         moves = board.available_moves(self.player)
 
         initial_score = board.get_player_goal(self.player)
         score_move_holder = {}
 
         # Examine all moves
-        #print(f'Available moves for player {self.player}: {moves}')
         for move in moves:
-            board_copy = deepcopy(board)
-            board_copy.position = move
-            board_copy.side     = self.player
-            first_move = True
-            #print(f'Trying move {move} for player {board_copy.player} on side {board_copy.side}')
-            while True:
-                #print(board_copy)
-                if not first_move:
-                    # ended in player goal
-                    if board_copy.side == None and board_copy.position == 0:
-                        #print('Ended in player goal')
-                        #print(board_copy)
-                        break
-                    # ended in empty bucket, players switch
-                    elif board_copy.last_bucket_empty(board_copy.side, board_copy.position):
-                        #print('Ended in empty bucket')
-                        #print(board_copy)
-                        break
-                board_copy.make_player_move(board_copy.player, board_copy.position, board_copy.side)
-                first_move = False
-            score_move_holder[move] = self.move_score(board_copy) - initial_score
+            score = self.get_move_score(move, board)
+            score_move_holder[move] = score
 
         max_score = max(score_move_holder.values())
-        #print(score_move_holder)
-        #print(max_score)
 
         # find max score, and corresponding move
         choices = []
@@ -76,5 +51,23 @@ class MaxScorePlayer(Player):
         else:
             choice = self.rng.choice(choices)
 
-        #print(f'Move choice: {choice}')
         return choice
+
+    def get_move_score(self, move, board):
+        from copy import deepcopy
+        board_copy = deepcopy(board)
+        board_copy.position = move
+        board_copy.side     = self.player
+        first_move = True
+
+        while True:
+            # ended in player goal
+            if board_copy.side == None and board_copy.position == 0:
+                break
+            if not first_move:
+                # ended in empty bucket, players switch
+                if board_copy.last_bucket_empty(board_copy.side, board_copy.position):
+                    break
+            board_copy.make_player_move(board_copy.player, board_copy.position, board_copy.side)
+            first_move = False
+        return self.score(board_copy)
